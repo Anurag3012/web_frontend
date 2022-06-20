2b0ed47becd846e0ed20025395df49d339384a85
Anurag3012 <100462923+Anurag3012@users.noreply.github.com>
2022-06-20 09:44:15 +0530

    new

--- index.php
+++ index.php
@@ -5,75 +5,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="navbar.css">
-    <title>Ak</title>
+    <link rel="stylesheet" href="Css/1.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+    <title>Columbia</title>
 </head>
 
 <body>
-    <div class="background_img">
-        <img src="columbia.jpg" alt="Loading">
-    </div>
-    <div class="container">
-        <nav>
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">about</a></li>
-                <li><a href="">contact us</a></li>
-                <li><a href="">Services</a></li>
-            </ul>
-            <div class="search">
-                <input type="text" id="search" placeholder="Search">
+    <header>
+        <?php include("../Anurag/Components/navbar.php") ?>
+        <div class="para">
+            <h1 >Online test</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab suscipit rerum, distinctio, voluptas odio temporibus dolorum ea velit error quo ut pariatur, nostrum sint deserunt voluptatem asperiores deleniti voluptatum! Dicta voluptatum exercitationem quaerat quae deserunt eaque omnis fuga reiciendis ipsum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque ipsa hic quibusdam ab distinctio voluptatum exercitationem aliquid quo tempore dolorem autem est molestias, quod at obcaecati provident, doloribus iure.
+            </p>
+            <div class="button">
+                <a href="#" class="btn">Login</a>
+                <a href="#" class="btn">Registor</a>
             </div>
-        </nav>
-    </div>
-    <div class="main">
-
-        <form action="">
-            <span>
-                <h3>Signup</h3>
-            </span>
-            <!-- <br> -->
-            <div class="upper">
-                <span class="font" >User type*</span> <br>
-                <div class="check">
-                    <span class="checkbox_mar">
-                        <input type="checkbox" name="check" id="checkbox1"><span> Student </span>
-                    </span>
-                    <input type="checkbox" name="check" id="checkbox2">
-                    <span> Department </span>
-                </div>
-            </div>
-            <!-- <br> -->
-            <div class="mid">
-                <div>
-                    <label for="email">
-                        <span class="font" >Email*</span><br><input type="email" name="email"
-                            class="placeholder" id="email" placeholder="Enter your email">
-                    </label>
-                </div>
-                <div>
-                    <label for="password">
-                        <span class="font" >Password*</span><br><input type="password" class="placeholder"
-                            name="pswd" id="pswd" placeholder="Enter your password">
-                    </label>
-                </div>
-                <div class="confirm">
-                    <label for="confirm">
-                        Confirm <input type="checkbox" name="confirm" id="con">
-                    </label>
-                </div>
-            </div>
-            <div class="lower">
-                <label for="submit">
-                    <input type="button" class="btn1" value="Submit">
-                </label>
-                <label for="reset">
-                    <input type="button" class="btn1" value="Reset">
-                </label>
-            </div>
-
-        </form>
-    </div>
+        </div>
+    </header>
 </body>
 
 </html>
