05ae18ec35795e546a03095ce0cf6b53bb267506
Anurag3012 <100462923+Anurag3012@users.noreply.github.com>
2022-06-24 11:53:44 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Css/1.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
@@ -14,14 +14,20 @@
 
 <body>
     <header>
-        <?php include("../Anurag/Components/navbar.php") ?>
+        <?php include("../Anurag/components/navbar.php") ?>
+        <div class="main">
+            <div class="logo">
+                <img src="columbia.jpg" alt="loading...">
+            </div>
+            <h1>Columbia</h1>
+        </div>
         <div class="para">
             <h1 >Online test</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab suscipit rerum, distinctio, voluptas odio temporibus dolorum ea velit error quo ut pariatur, nostrum sint deserunt voluptatem asperiores deleniti voluptatum! Dicta voluptatum exercitationem quaerat quae deserunt eaque omnis fuga reiciendis ipsum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Neque ipsa hic quibusdam ab distinctio voluptatum exercitationem aliquid quo tempore dolorem autem est molestias, quod at obcaecati provident, doloribus iure.
             </p>
             <div class="button">
                 <a href="#" class="btn">Login</a>
-                <a href="#" class="btn">Registor</a>
+                <a href="#" class="btn">Register</a>
             </div>
         </div>
     </header>
